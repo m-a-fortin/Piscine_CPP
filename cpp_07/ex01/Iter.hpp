5f57b6eb612267e510ddd12fa3dc3db2d05c6ed7
Marc-andre Fortin <mafortin@c3r12p2.42quebec.com>
2022-05-18 16:36:31 -0400

    comment added for cpp07ex01 eval

--- cpp_07/ex01/Iter.hpp
+++ cpp_07/ex01/Iter.hpp
@@ -2,8 +2,10 @@
 #pragma once
 #include <iostream>
 
+//DANS LE TEST DE L'EVAL, ON NOUS FORCE A METTRE T& COMME CONST, MAIS CA VA A L'ENCONTRE DU ITER.
+//CHANGER T& COMME CONST SEULEMENT POUR LE MAIN DE L'EVALUATION.
 template<typename T>
-void	iter(T* adress, unsigned int size, void (*f)(T&)){
+void	iter(T* adress, unsigned int size, void f(T&)){
 	for (unsigned int i = 0; i < size; i++){
 		f(adress[i]);
 	}
--- cpp_07/ex01/main.cpp
+++ cpp_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mafortin <mafortin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 13:23:52 by mafortin          #+#    #+#             */
-/*   Updated: 2022/05/17 15:21:24 by mafortin         ###   ########.fr       */
+/*   Updated: 2022/05/18 16:31:38 by mafortin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 int	main(void){
 	std::string tab[3] = {"string 1", "string 2", "string 3"};
 	std::cout << "PRINTING ARRAY OF STD::STRING\n";
-	::iter(tab, 3, funct_print);
+	iter(tab, 3, funct_print);
 	int	tab2[10] = {0, 1, 2, 3, 4 , 5, 6, 7, 8, 9};
 	std::cout << "\nPRINTING ARRAY OF INT\n";
-	::iter(tab2, 10, funct_print);
+	iter(tab2, 10, funct_print);
 	std::cout << "INCREASING BY 1 EACH ELEMENT OF ARRAY OF INT\n";
-	::iter(tab2, 10, funct_add);
+	iter(tab2, 10, funct_add);
 	std::cout << "PRINTING RESULT\n";
 	iter(tab2, 10, funct_print);
 }
